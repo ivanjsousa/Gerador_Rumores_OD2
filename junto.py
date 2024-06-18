a6d13b7a23b70731b984e0e513990c860dfb7350
Ivan Sousa <ivanjsousa@gmail.com>
2024-06-18 14:30:30 -0300

    nova versão codigo unico para todos os geradores

--- junto.py
+++ junto.py
@@ -1,7 +1,7 @@
 import random
 import flet as ft
 
-# Dados
+# Dados para o primeiro gerador de rumores
 QUANDO = ['na semana passada', 'no último inverno', 'três dias atrás', 'pela manhã', 'depois da meia noite',
           'hoje mais cedo', 'durante o almoço', 'algumas horas atrás', 'durante a tarde', 'no jantar']
 
@@ -33,8 +33,28 @@ PISTA3 = ['está recrutando um grupo de heróis para investigar', 'paga pela con
           'tem interesse na história', 'contratou bandidos para se proteger', 'enviou aventureiros para lá',
           'afirma que é mentira', 'investiga com atenção', 'tenta abafar o caso']
 
+# Dados para o segundo gerador de rumores
+masmorra = ["um Mago Louco", "um Mago Ancestral", "um Clérigo Cego", "um Clérigo Poderoso", "um Clérigo Proscrito",
+            "um Guerreiro Poderoso", "um Guerreiro Rico", "um Rei Antigo", "um Rei Poderoso", "um Rei Furioso",
+            "um Rei", "uma Sacerdotisa", "um Sábio", "um Demônio", "a Arak-Takna", "um Dragão", "um Orc",
+            "um Vampiro", "um Lich", "um Druida", "um Sacerdote", "o próprio Orcus", "um Herói", "um Antigo Vilão",
+            "um Comerciante", "um Ladrão ambicioso", "um Ladrão enriquecido", "um Príncipe", "uma Rainha",
+            "um Senhor do crime", "um Cultista", "um Bardo", "uma Bruxa", "um Ser Amaldiçoado", "uma Marilith",
+            "um Devorador de Cérebro"]
 
-def gerar_rumor():
+Para = ["proteger", "esconder", "aprisionar", "deter", "impedir", "descobrir", "derrotar", "explorar", "vigiar",
+        "atrair", "contratar", "selecionar", "eliminar", "cultuar", "invocar", "restaurar", "enviar", "explodir",
+        "criar", "formar", "idolatrar", "conjurar", "afastar", "libertar", "aguardar", "transportar", "seguir",
+        "recuperar", "confinar", "invocar", "reviver", "expulsar", "raptar", "definhar", "apagar", "sumir"]
+
+Uma = ["tesouro", "monstro", "espada", "grimório", "Ídolo", "deus", "documento", "demônio", "lich", "joia", "trono",
+       "escravo", "vampiro", "profecia", "Líder religioso", "Arak-Takna", "Orcus", "Cthulhu", "ovo de dragão", "portal",
+       "rival", "seita", "armadura", "mapa", "goblin", "drider", "criatura", "magia", "elmo", "Guilda", "ettin", "orc",
+       "opositor", "confraria", "adaga", "gigante"]
+
+
+# Funções para gerar rumores
+def gerar_rumor_tipo_1():
     rumor = (f" ''Vocês ouviram {random.choice(QUANDO)}, que {random.choice(QUEM)} {random.choice(ATIVIDADE)} "
              f"{random.choice(COISA)} {random.choice(QUALIDADE)} {random.choice(ONDE)} "
              f"e junto havia {random.choice(PISTA1)}. "
@@ -42,21 +62,37 @@ def gerar_rumor():
     return rumor
 
 
+def gerar_rumor_tipo_2():
+    rumor = (f" ''A masmorra foi criada por {random.choice(masmorra)} para {random.choice(Para)} um(a) "
+             f"{random.choice(Uma)}.'' ")
+    return rumor
+
+
+# Função principal
 def main(page: ft.Page):
     page.title = "Gerador de Rumores"
-    page.window_width = 450
-    page.window_height = 305
+    page.window_width = 500
+    page.window_height = 400
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    rumor_text = ft.Text(gerar_rumor(), size=20, text_align=ft.TextAlign.JUSTIFY)
+    rumor_text = ft.Text(size=20, text_align=ft.TextAlign.JUSTIFY)
 
     def on_generate_click(event):
-        rumor_text.value = gerar_rumor()
+        if dropdown.value == "Rumor OD2":
+            rumor_text.value = gerar_rumor_tipo_1()
+        else:
+            rumor_text.value = gerar_rumor_tipo_2()
         page.update()
 
     def on_copy_click(event):
         page.set_clipboard(rumor_text.value)
+
+    dropdown = ft.Dropdown(
+        options=[ft.dropdown.Option("Rumor OD2"), ft.dropdown.Option("Rumor Solo Dragon")],
+        value="Rumor OD2",
+        width=170
+    )
 
     generate_button = ft.ElevatedButton(text="Gerar Novo Rumor", on_click=on_generate_click)
     copy_button = ft.ElevatedButton(text="Copiar Texto", on_click=on_copy_click)
@@ -64,6 +100,8 @@ def main(page: ft.Page):
     page.add(
         ft.Column(
             [
+                dropdown,
+                ft.Container(height=20),  # Espaçamento entre o dropdown e o texto
                 rumor_text,
                 ft.Container(height=20),  # Espaçamento entre o texto e os botões
                 ft.Row(
